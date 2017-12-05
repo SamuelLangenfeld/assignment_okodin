331309b24fe08beb1c6ea9e258581eb8d342ea71
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-12-05 11:48:08 -0500

    users show and index functinal

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": "assignment_okodin_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": "assignment_okodin_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "assignment_okodin_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
---
+++ routes/users.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var models = require("../models");
+var User = models.User;
+var sequelize = models.sequelize;
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  User.findAll({}).then(users => {
+      let arrayOne = [];
+      users.map((user, i) => {
+        arrayOne[Math.floor(i / 3)] = arrayOne[Math.floor(i / 3)] || [];
+        arrayOne[Math.floor(i / 3)][i % 3] = user;
+      })
+      res.render('usersIndex', { arrayOne });
+    })
+    .catch(e => {
+      console.error(e);
+    })
+
+});
+
+router.get('/:id', function(req, res, next) {
+  User.findById(Number(req.params.id)).then(user => {
+      res.render('user', { user });
+    })
+    .catch(e => {
+      console.error(e);
+    })
+
+});
+
+module.exports = router;
